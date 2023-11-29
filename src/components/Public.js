7494673fb097fc220bda0b691a294632faadf884
Rx-11 <110684815+Rx-11@users.noreply.github.com>
2023-11-30 00:46:26 +0530

    Update Public.js

--- src/components/Public.js
+++ src/components/Public.js
@@ -5,21 +5,15 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
+                <h1><span className="nowrap">TechNotes</span></h1>
             </header>
             <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
-                <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
-                </address>
+                <p>This app is made using the MERN stack</p>
                 <br />
-                <p>Owner: Dan Davidson</p>
+                <p>Made by Vedant Kannawar</p>
             </main>
             <footer>
-                <Link to="/login">Employee Login</Link>
+                <Link to="/login">Login</Link>
             </footer>
         </section>
 
